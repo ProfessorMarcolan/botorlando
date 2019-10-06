3a1ba06bb234517ce9f292faee70397b9c56eb95
Wagner Riffel <wgrriffel@gmail.com>
2019-10-06 11:55:36 -0300

    tests/ircgen: strtok was corrupting the data, use a copy instead.
    Signed-off-by: Wagner Riffel <wgrriffel@gmail.com>

--- tests/ircgen.c
+++ tests/ircgen.c
@@ -5,18 +5,16 @@
 
 #include "../htab.c"
 
-static const char *hentsdecl =
-	"static const Hent ircents[] = {";
-static const char *hentsrow =
-	"\t[%u] = { .hash=%u, .key=\"%s\" .data=%s }\n";
-static const char *hentsclose =
-	"};";
-static const char *htabfmt =
-	"\nstatic const Htab irctab = {\n"
-	"\t{.max = %u, .nents = %u, .ents = ircents}\n"
-	"};\n";
+static const char *hentsdecl = "static const Hent ircents[%u] = {\n";
+static const char *hentsrow = "\t[%u] = { .hash=%u, .key=\"%s\" .data=%s },\n";
+static const char *hentsclose = "};";
+static const char *htabfmt = "\nstatic const Htab irctab = {\n"
+			     "\t{.max = %u, .nents = %u, .ents = ircents}\n"
+			     "};\n";
 
-int main(void){
+int
+main(void)
+{
 	Htab *t;
 	FILE *f;
 	char *buf, *irc, *fn, *prev;
@@ -24,57 +22,51 @@ int main(void){
 	int ecode;
 
 	ecode = EXIT_FAILURE;
+	n = 8192;
 	t = NULL;
 	f = fopen("irctab.data", "r");
-	if(f == NULL){
+	if (f == NULL) {
 		perror("ircgen");
 		goto err;
 	}
-	buf = malloc(8192);
-	if(buf == NULL){
+	buf = malloc(n);
+	if (buf == NULL) {
 		perror("ircgen");
 		goto err;
 	}
-
-        /* ignoring short reads */
-	n = fread(buf, 1, 8192, f);
-	if(ferror(f) != 0) {
-		/* error is not eof */
-		if(feof(f) == 0) {
-			perror("ircgen");
-			goto err;
-		}
-	}
-	buf[n] = '\0';
 
 	t = Hmake();
-	for(irc=strtok_r(buf, ",", &prev); irc != NULL; irc=strtok_r(NULL, ",", &prev)) {
-		printf("irc: %s\n", irc);
-//		fn = strtok(irc, "\n");
+	while (getline(&buf, &n, f) > 0) {
+		char *tmpirc, *tmpfn;
 
-		if(fn == NULL) {
-			fprintf(stderr, "ircgen: function not found");
+		irc = strtok(buf, ",");
+		if (irc == NULL) {
+			fprintf(stderr, "ircgen: badly formatted file\n");
 			goto err;
 		}
-		Hput(t, irc, NULL);
-	}
-	exit(1);
-	puts(hentsdecl);
-	for(i=0; i < t->max; i++) {
-		if(t->ents[i].hash) {
-			printf(hentsrow, i,
-			       t->ents[i].hash,
-			       t->ents[i].key,
-			       t->ents[i].data);
+		fn = strtok(NULL, "\n");
+		if (fn == NULL) {
+			fprintf(stderr, "ircgen: badly formatted file\n");
+			goto err;
 		}
+		tmpirc = malloc(strlen(irc) + 1);
+		tmpfn = malloc(strlen(fn) + 1);
+		Hput(t, strcpy(tmpirc, irc), strcpy(tmpfn, fn));
 	}
+
+	printf(hentsdecl, t->max);
+	for (i = 0; i < t->max; i++) {
+		if (t->ents[i].hash)
+			printf(hentsrow, i, t->ents[i].hash, t->ents[i].key,
+			       t->ents[i].data);
+	}
+
 	puts(hentsclose);
 	printf(htabfmt, t->max, t->nents, t->ents);
 	ecode = EXIT_SUCCESS;
+
 err:
-	if(t != NULL)
-		Hfree(t);
-	if(f != NULL)
+	if (f != NULL)
 		fclose(f);
 
 	fflush(stderr);
